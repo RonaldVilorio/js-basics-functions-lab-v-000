aad6fd9e8559140b69cf138a316742d4104f0c89
ronald vilorio <soccer23415@gmail.com>
2018-05-12 20:03:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function distanceFromHqInFeet(distance){
 function distanceTravelledInFeet(distance){
   // value a is distance b to c
   // value b is distance a to b
-  return distance * 264
+  return distance 
 
 
 }
